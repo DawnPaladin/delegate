734917c0ddf28402a63341b579aea04dd9e7eea3
James Harris <computersherpa@gmail.com>
2020-06-02 18:09:45 -0500

    Report task completion

--- features/delegate.js
+++ features/delegate.js
@@ -10,7 +10,7 @@
 
 module.exports = function(controller) {
 	controller.on('slash_command', async(bot, message) => {
-		const sendingUserId = message.user_id;
+		const assigner = message.user_id;
 		
 		const messageParts = message.text.match(/<@(U\w+)\|(\w+)>\s*(.*)/); // regexr.com/55tlg
 		
@@ -19,38 +19,51 @@ module.exports = function(controller) {
 			await bot.replyPrivate(message, "😞 Sorry, I didn't catch that. I need you to ask me like this: */delegate @user Do the thing*");
 			
 		} else {
-			const receivingUserId = messageParts[1];
+			console.log(message);
+			const worker = messageParts[1];
 			const task = messageParts[3];
 
-			await bot.replyPublic(message, `"${task}" has been delegated to <@${receivingUserId}>.`);
+			await bot.replyPublic(message, `"${task}" has been delegated to <@${worker}>.`);
 
-			await bot.startPrivateConversation(receivingUserId);
+			await bot.startPrivateConversation(worker);
 			
 			// TODO: Add context link (https://api.slack.com/methods/chat.getPermalink)
 			
-			const blocks = {
+			await bot.say({
 				"blocks": [
 					{
 						"type": "section",
 						"text": {
 							"type": "mrkdwn",
-							"text": `<@${sendingUserId}> has a new task for you: ${task}`,
+							"text": `<@${assigner}> has a new task for you: ${task}`,
 						},
 						"accessory": {
 							"type": "button",
 							"text": {
 								"type": "plain_text",
-								"text": "✔ Complete",
+								"text": "Complete",
 								"emoji": true
 							},
-							"value": "click_me_123"
+							"value":  message.channel_id + "|" + assigner + "|" + task
 						}
 					}
 				]
-			}
-			
-			await bot.say(blocks);
+			});
 			
 		}
 	});
+	
+	controller.on('block_actions', async(bot, message) => {
+		console.log(message);
+		
+		// Button value looks like this: channelId|assigner|task
+		const messageArray = message.text.split('|');
+		const channelId = messageArray.shift();
+		const assigner = messageArray.shift();
+		const task = messageArray.join('|'); // in case task has any pipe characters in it
+		const worker = message.user;
+		
+		await bot.startConversationInChannel(channelId);
+		await bot.say(`<@${worker}> reports that the task <@${assigner}> delegated to them is complete: ${task}`)
+	})
 }
