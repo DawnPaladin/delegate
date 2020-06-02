2994a0b72ec1d6eeafd23c1232163134bfc35e59
James Harris <computersherpa@gmail.com>
2020-06-01 21:58:29 -0500

    Accept a task via slash command from one user and message it to another

--- features/delegate.js
+++ features/delegate.js
@@ -1,19 +1,31 @@
-const { SlackDialog } = require('botbuilder-adapter-slack');
+// Workspace: https://app.slack.com/client/T014GSPLNH2/C014FQ47P26
+// Bot controls: https://api.slack.com/apps/A0150GPN4BS
+
+// Plan for no-database system is to receive a message from one user and immediately pass it to another
+// Persistence options: https://botkit.ai/docs/v4/core.html#enable-conversation-persistence
+// https://www.npmjs.com/package/botbuilder-storage-mongodb
+// Or just use process.env.MONGO_URI in bot.js
+
+// const { SlackDialog } = require('botbuilder-adapter-slack');
 
 module.exports = function(controller) {
 	controller.on('slash_command', async(bot, message) => {
-		if (message.text === 'plain') {
-			await bot.reply(message, 'This is a plain reply');
-		} else if (message.text === 'public') {
-			await bot.replyPublic(message, 'This is a public reply');
-		} else if (message.text === 'private') {
-			await bot.replyPrivate(message, 'This is a private reply');
-		}
+		const sendingUserId = message.user_id;
 		
-		console.log(message);
+		const messageParts = message.text.match(/<@(U\w+)\|(\w+)>\s*(.*)/); // regexr.com/55tlg
+		
+		if (!messageParts || !messageParts[3]) {
+			
+			await bot.replyPrivate(message, "😞 Sorry, I didn't catch that. I need you to ask me like this: */delegate @user Do the thing*");
+			
+		} else {
+			const receivingUserId = messageParts[1];
+			const task = messageParts[3];
 
-		// set http status
-		bot.httpBody({text:'You can send an immediate response using bot.httpBody()'});
+			bot.httpBody({text:`Got it. I'll ask <@${receivingUserId}> to "${task}" and report back when they're done.`});
 
+			await bot.startPrivateConversation(receivingUserId);
+			await bot.say(`<@${sendingUserId}> has a new task for you: ${task}`);
+		}
 	});
 }
